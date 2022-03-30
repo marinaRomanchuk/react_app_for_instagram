95f50dca2da570800733c06883e4301dbf40694a
Marina <marina.romanchuk23@gmail.com>
2022-03-30 16:12:02 +0300

    adding post's author

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -11,22 +11,27 @@ import comment_empty  from '../comment.png';
 import comment_full  from '../comment_full.png';
 
 
-async function getListOfPosts(token) {
-    const AuthStr = 'Token '.concat(token);
+async function getListOfPosts(AuthStr) {
     return axios.get("http://" + API_DOMAIN + "/api/posts/", { headers: { Authorization: AuthStr }, params: { feed: true,}
     })
         .then(result => result.data);
 }
 
-async function getListOfComments(token, post_id) {
-    const AuthStr = 'Token '.concat(token);
+async function getListOfComments(AuthStr, post_id) {
     return axios.get("http://" + API_DOMAIN + "/api/comments/", { headers: { Authorization: AuthStr }, params: { post_id: post_id,}
     })
         .then(result => result.data);
 }
 
-function postComment(token, credentials) {
-    const AuthStr = 'Token '.concat(token);
+async function getUser(AuthStr, user_id) {
+    const user_str = ''.concat(user_id);
+    return axios.get("http://" + API_DOMAIN + "/api/users/" + user_str + "/",
+        { headers: { Authorization: AuthStr }, params: { feed: true,}
+    })
+        .then(result => result.data);
+}
+
+function postComment(AuthStr, credentials) {
     return axios.post("http://" + API_DOMAIN + "/api/comments/", credentials, {
         headers: {Authorization: AuthStr}
     }).catch((error) => {
@@ -34,8 +39,7 @@ function postComment(token, credentials) {
     });
 }
 
-function postLike(token, post_id, is_like) {
-    const AuthStr = 'Token '.concat(token);
+function postLike(AuthStr, post_id, is_like) {
     const post_str = ''.concat(post_id);
     if (is_like) {
         return axios.post("http://" + API_DOMAIN + "/api/posts/" + post_str + "/like/", {}, {
@@ -53,8 +57,7 @@ function postLike(token, post_id, is_like) {
     }
 }
 
-function deleteLike(token, post_id, is_like) {
-    const AuthStr = 'Token '.concat(token);
+function deleteLike(AuthStr, post_id, is_like) {
     const post_str = ''.concat(post_id);
     if (is_like) {
         return axios.delete("http://" + API_DOMAIN + "/api/posts/" + post_str + "/like/",  {
@@ -72,8 +75,7 @@ function deleteLike(token, post_id, is_like) {
     }
 }
 
-async function getLikesCount(token, post_id) {
-    const AuthStr = 'Token '.concat(token);
+async function getLikesCount(AuthStr, post_id) {
     const post_str = ''.concat(post_id);
     return axios.get("http://" + API_DOMAIN + "/api/posts/" + post_str + "/likes/count/", { headers: { Authorization: AuthStr }
     })
@@ -84,17 +86,20 @@ function Feed(props) {
     const { token, setToken } = useToken();
     const [ postList, setPostList ] = useState([]);
     const [ loading, setLoading ] = useState(true);
+    const AuthStr = 'Token '.concat(token);
 
     useEffect(() => {
         const handleSome = async () => {
             setLoading(true);
-            const posts = await getListOfPosts(token);
+            const posts = await getListOfPosts(AuthStr);
 
             let ind = 0;
             for (let item of posts) {
-                const likes = await getLikesCount(token, item.id);
-                const comments = await getListOfComments(token, item.id);
+                const likes = await getLikesCount(AuthStr, item.id);
+                const comments = await getListOfComments(AuthStr, item.id);
+                const user = await getUser(AuthStr, item.user);
 
+                posts[ind].user = user;
                 posts[ind].likes_count = likes.likes_count;
                 posts[ind].dislikes_count = likes.dislike_count;
                 posts[ind].like = likes.like;
@@ -107,25 +112,23 @@ function Feed(props) {
             setLoading(false);
         }
         handleSome();
-        console.log(postList);
-
     }, []);
 
     const clickLike = (post_id, ind) => {
         const newPostList = postList.postList;
 
         if (newPostList[ind].like) {
-            deleteLike(token, post_id, 1);
+            deleteLike(AuthStr, post_id, 1);
             newPostList[ind].like = false;
             newPostList[ind].likes_count -= 1;
         }
         else {
             if (newPostList[ind].dislike) {
-                deleteLike(token, post_id, 0);
+                deleteLike(AuthStr, post_id, 0);
                 newPostList[ind].dislikes_count -= 1;
                 newPostList[ind].dislike = false;
             }
-            postLike(token, post_id, 1);
+            postLike(AuthStr, post_id, 1);
             newPostList[ind].like = true;
             newPostList[ind].likes_count += 1;
         }
@@ -137,17 +140,17 @@ function Feed(props) {
         const newPostList = postList.postList;
 
         if (newPostList[ind].dislike) {
-            deleteLike(token, post_id, 0);
+            deleteLike(AuthStr, post_id, 0);
             newPostList[ind].dislike = false;
             newPostList[ind].dislikes_count -= 1;
         }
         else {
             if (newPostList[ind].like) {
-                deleteLike(token, post_id, 1);
+                deleteLike(AuthStr, post_id, 1);
                 newPostList[ind].like = false;
                 newPostList[ind].likes_count -= 1;
             }
-            postLike(token, post_id, 0);
+            postLike(AuthStr, post_id, 0);
             newPostList[ind].dislike = true;
             newPostList[ind].dislikes_count += 1;
         }
@@ -164,13 +167,13 @@ function Feed(props) {
     const handleSubmit = async (e, post_id, ind) => {
         e.preventDefault();
 
-        await postComment(token,{
+        await postComment(AuthStr,{
             text: e.target.text.value,
             post: post_id
         });
 
         const newPostList = postList.postList;
-        const comments = await getListOfComments(token, post_id);
+        const comments = await getListOfComments(AuthStr, post_id);
         newPostList[ind].comments = comments;
         setPostList({postList: newPostList});
     }
@@ -181,9 +184,14 @@ function Feed(props) {
             {!loading && (
                 <div class="list-group">
                     <h2> Welcome to your feed!</h2>
-                        <center>
                          { postList.postList.map((item, index) => (
                              <div class="list-group-item list-group-item-action flex-column align-items-start">
+                                 <div>
+                                     <img src={item.user.profile_photo } width={40} style={{display:"inline-block"}}>
+                                     </img>
+                                     <h4 style={{display:"inline-block"}}>{ item.user.username }</h4>
+                                 </div>
+                                 <center>
                                  <img src={ "http://" + API_DOMAIN + item.photo } width={600}></img>
                                  <h3>{item.description}</h3>
                                  <button className="btn btn-secondary" onClick={() => clickLike(item.id, index)}>
@@ -198,6 +206,7 @@ function Feed(props) {
                                      <img src={item.show_comments ? comment_full : comment_empty}
                                           alt='comment' width={25}/>
                                  </button>
+                                 </center>
                                  { item.show_comments ? item.comments.map(comment => (
                                      <div class="list-group-item list-group-item-action">{comment.text}
                                      </div>
@@ -206,17 +215,16 @@ function Feed(props) {
                                      <form method="post"
                                            onSubmit={(e) => handleSubmit(e, item.id, index)}>
                                          <div className="form-group">
-                                             <fieldset>
-                                                 <input className="form-control" type="text" name="text"
-                                                        placeholder="Leave your comment" required="required"/>
-                                                 <button type="submit" className="btn btn-outline-dark">Send</button>
-                                             </fieldset>
+                                             <input className="form-control" type="text" name="text"
+                                                    placeholder="Leave your comment" required="required"/>
+                                             <br></br>
+                                             <button type="submit" className="btn btn-outline-dark">Send</button>
                                          </div>
                                      </form>
                                  </div>
                              </div>
                          ))}
-                        </center>
+
 
                 </div>
             )}
