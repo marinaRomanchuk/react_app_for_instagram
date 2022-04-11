c2dd3a6d621d0300f08b1f9852fde247b9f432e4
Marina <marina.romanchuk23@gmail.com>
2022-04-11 16:44:37 +0300

    adding profile page for all users

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import SignIn from './components/SignIn';
 import SignUp from './components/SignUp';
 import Profile from './components/Profile';
 import Search from './components/Search';
+import User from './components/User'
 import useToken from './components/useToken';
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import "./bootstrap.css";
@@ -23,6 +24,7 @@ function App() {
                     <Route path="/signin" element={<SignIn setToken={setToken}/>}/>
                     <Route path="/signup" element={<SignUp/>}/>
                     <Route path="/search" element={<Search/>}/>
+                    <Route path="/user/:id" element={<User/>}/>
                 </Routes>
             </div>
         </BrowserRouter>
--- src/components/User.js
+++ src/components/User.js
@@ -9,13 +9,21 @@ import dislikeFull  from '../dislike_full.png';
 import dislikeEmpty  from '../dislike_empty.png';
 import commentEmpty  from '../comment.png';
 import commentFull  from '../comment_full.png';
+import basket  from '../basket.png';
 import logo from '../inst_logo.png';
-import {Link} from "react-router-dom";
+import { useParams } from "react-router-dom";
+import PropTypes from "prop-types";
 
 
-async function getListOfPosts(authStr) {
+async function getListOfPosts(authStr, userId) {
     return axios.get("http://" + API_DOMAIN + "/api/posts/", { headers: { Authorization: authStr },
-        params: { feed: true,}})
+        params: { user_id: userId,}})
+        .then(result => result.data);
+}
+
+async function getUserInfo(authStr, userId) {
+    const userStr = ''.concat(userId);
+    return axios.get("http://" + API_DOMAIN + "/api/users/" + userStr + "/", { headers: { Authorization: authStr }})
         .then(result => result.data);
 }
 
@@ -69,16 +77,32 @@ function deleteLike(authStr, postId, isLike) {
     }
 }
 
-function Feed(props) {
+function deletePost(authStr, postId) {
+    const postStr = ''.concat(postId);
+    return axios.delete("http://" + API_DOMAIN + "/api/posts/" + postStr + "/",  {
+        headers: {Authorization: authStr}
+    }).catch((error) => {
+        console.log(error.response);
+    });
+}
+
+function User(props) {
     const { token, setToken } = useToken();
     const [ postList, setPostList ] = useState([]);
+    const [ user, setUser] = useState({});
     const [ loading, setLoading ] = useState(true);
     const authStr = 'Token '.concat(token);
+    const { id } = useParams();
 
     useEffect(() => {
         const handleSome = async () => {
             setLoading(true);
-            const posts = await getListOfPosts(authStr);
+            console.log(id);
+            console.log(''.concat(id));
+            const user = await getUserInfo(authStr, id);
+            setUser(user);
+
+            const posts = await getListOfPosts(authStr, user.id);
             for (let post of posts) {
                 post.showComments = false;
             }
@@ -87,6 +111,7 @@ function Feed(props) {
         }
         handleSome();
     }, []);
+
 
     const clickLike = (postId, index) => {
         const newPostList = postList.postList;
@@ -128,7 +153,6 @@ function Feed(props) {
             newPostList[index].stats.has_disliked = true;
             newPostList[index].stats.dislikes_count += 1;
         }
-
         setPostList({postList: newPostList});
     }
 
@@ -163,66 +187,69 @@ function Feed(props) {
             {loading && <div>Loading</div>}
             {!loading && (
                 <div class="list-group" style={{maxWidth: "1000px", margin:"0 auto"}}>
-                    <h2> Welcome to your feed!</h2>
-                     { postList.postList.map((post, index) => (
-                         <div class="list-group-item list-group-item-action flex-column align-items-start">
-                             <div>
-                                 <img src={ post.author.profile_photo ? post.author.profile_photo : logo } width={40}
-                                      style={{display: "inline-block"}}>
-                                 </img>
-                                 <h4 style={{display: "inline-block", margin: "5px"}}> <Link to={{
-                                     pathname: `/user/${post.author.id}`,
-                                     state: { id: post.author.id }
-                                 }}>{ post.author.username }</Link></h4>
-                             </div>
-                             <center>
-                             <img src={ post.photo } width={600} alt={"image"}></img>
-                             <h3>{post.description}</h3>
-                             <button className="btn btn-secondary" onClick={() => clickLike(post.id, index)}
-                                     style={{margin: "3px", height: "50px"}}>
-                                 <img src={post.stats.has_liked ? likeFull : likeEmpty} alt='like' width={25}
-                                      style={{margin: "3px"}}/>
-                                 {post.stats.likes_count}
-                             </button>
-                             <button className="btn btn-secondary" onClick={() => clickDislike(post.id, index)}
-                                     style={{margin: "3px", height: "50px"}}>
-                                 <img src={post.stats.has_disliked ? dislikeFull : dislikeEmpty} alt='dislike' width={25}
-                                      style={{margin: "3px"}}/>
-                                 {post.stats.dislikes_count}
-                             </button>
-                             <button className="btn btn-secondary" onClick={() => clickComments(index)}
-                                     style={{margin: "3px", height: "50px"}}>
-                                 <img src={post.showComments ? commentFull : commentEmpty}
-                                      alt='comment' width={25} style={{margin: "3px"}}/>
-                                 {post.stats.comments_count}
-                             </button>
-                             </center>
-                             { post.showComments ? post.comments.map(comment => (
-                                 <div class="list-group-item list-group-item-action">
-                                     <img src={ comment.user_info.profile_photo ? "http://" + API_DOMAIN +
-                                         comment.user_info.profile_photo : logo } width={30}
-                                          style={{display:"inline-block"}}>
-                                     </img>
-                                     <p style={{display:"inline-block", margin:"5px"}}><Link to={{
-                                         pathname: `/user/${comment.user_info.id}`,
-                                         state: { id: comment.user_info.id }
-                                     }}>{ comment.user_info.username }</Link></p>
-                                     <h5 style={{display:"inline-block"}}>{ comment.text }</h5>
-                                 </div>
-                             )) : <div></div>}
-                             <div className="list-group-item list-group-item-action">
-                                 <form method="post"
-                                       onSubmit={(e) => handleSubmit(e, post.id, index)}>
-                                     <div className="form-group">
-                                         <input className="form-control" type="text" name="text"
-                                                placeholder="Leave your comment" required="required"/>
-                                         <br></br>
-                                         <button type="submit" className="btn btn-outline-dark">Send</button>
-                                     </div>
-                                 </form>
-                             </div>
-                         </div>
-                     ))}
+                    <div>
+                        <img src={ user.profile_photo ? user.profile_photo : logo } width={100} alt={"image"}
+                             style={{display: "inline-block", margin: "5px"}}/>
+                        <h2 style={{display: "inline-block", margin: "10px"}}>{ user.username }</h2>
+                    </div>
+                    <h3> { user.first_name + " " + user.last_name } </h3>
+                    <h4> { user.date_of_birth } </h4>
+                    <h4> { user.description } </h4>
+
+                    <br></br>
+                    { postList.postList.map((post, index) => (
+                        <div class="list-group-item list-group-item-action flex-column align-items-start">
+                            <div style={{display: "inline-block", width: "100%"}}>
+                                <img src={ post.author.profile_photo ? post.author.profile_photo : logo } width={40}
+                                     style={{display: "inline-block"}}>
+                                </img>
+                                <h4 style={{display: "inline-block", margin: "5px"}}>{ post.author.username }</h4>
+                            </div>
+                            <center>
+                                <img src={ post.photo } width={600} alt={"image"}/>
+                                <h3>{post.description}</h3>
+                                <button className="btn btn-secondary" onClick={() => clickLike(post.id, index)}
+                                        style={{margin: "3px", height: "50px"}}>
+                                    <img src={post.stats.has_liked ? likeFull : likeEmpty} alt='like' width={25}
+                                         style={{margin: "3px"}}/>
+                                    {post.stats.likes_count}
+                                </button>
+                                <button className="btn btn-secondary" onClick={() => clickDislike(post.id, index)}
+                                        style={{margin: "3px", height: "50px"}}>
+                                    <img src={post.stats.has_disliked ? dislikeFull : dislikeEmpty} alt='dislike' width={25}
+                                         style={{margin: "3px"}}/>
+                                    {post.stats.dislikes_count}
+                                </button>
+                                <button className="btn btn-secondary" onClick={() => clickComments(index)}
+                                        style={{margin: "3px", height: "50px"}}>
+                                    <img src={post.showComments ? commentFull : commentEmpty}
+                                         alt='comment' width={25} style={{margin: "3px"}}/>
+                                    {post.stats.comments_count}
+                                </button>
+                            </center>
+                            { post.showComments ? post.comments.map(comment => (
+                                <div class="list-group-item list-group-item-action">
+                                    <img src={ comment.user_info.profile_photo ? "http://" + API_DOMAIN +
+                                        comment.user_info.profile_photo : logo } width={30}
+                                         style={{display:"inline-block"}}>
+                                    </img>
+                                    <p style={{display:"inline-block", margin:"5px"}}>{ comment.user_info.username }</p>
+                                    <h5 style={{display:"inline-block"}}>{ comment.text }</h5>
+                                </div>
+                            )) : <div></div>}
+                            <div className="list-group-item list-group-item-action">
+                                <form method="post"
+                                      onSubmit={(e) => handleSubmit(e, post.id, index)}>
+                                    <div className="form-group">
+                                        <input className="form-control" type="text" name="text"
+                                               placeholder="Leave your comment" required="required"/>
+                                        <br></br>
+                                        <button type="submit" className="btn btn-outline-dark">Send</button>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    ))}
                 </div>
             )}
         </div>
@@ -230,4 +257,4 @@ function Feed(props) {
 }
 
 
-export default Feed;
+export default User;
